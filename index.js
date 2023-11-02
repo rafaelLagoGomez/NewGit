1468d884dfc70a14970b8572f30ba83852fd8621
= <=>
2023-11-03 00:23:44 +0100

    reto1

---
+++ index.js
@@ -0,0 +1,31 @@
+
+//Reto2-Clase5
+
+function sum (number1, number2) {
+    return number1 + number2;
+}
+
+function sums (number1, number2) {
+    return number1 - number2;
+}
+
+function mult (number1, number2) {
+    return number1 * number2;
+}
+
+function div (number1, number2) {
+    return number1 / number2;
+}
+
+function cuadr (number1) {
+    return number1**2;
+}
+
+module.exports = {
+    sum, 
+    sums, 
+    mult, 
+    div,
+    cuadr
+}
+
